203e1ec00b73754eb2f5bd6fec1e3ca3f9e6ae4d
Shahram Adalat Mohammed <shahramadalat@gmail.com>
2025-12-30 11:08:51 +0300

    saved point which works

---
+++ android/app/src/main/kotlin/com/shahramadalat/times/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.shahramadalat.times
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/shahrdev/times/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.shahrdev.times
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
